5ab36efdca7a633d37589f3ae6d037f680b35a86
Farhan Ahmed <azzassfa@gmail.com>
2022-11-05 16:05:25 -0700

    Change fopen to open and started reading required size from the end.

--- copylog.cpp
+++ copylog.cpp
@@ -1,5 +1,32 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// Edited: note comments after the code
+#ifndef O_LARGEFILE
+#define O_LARGEFILE 0
+#endif
+
+int read_from_file_open(char *filename,size_t size)
+{
+    int fd, sz; 
+    char *c = (char *) calloc(100, sizeof(char)); 
+    
+    fd = open(filename, O_RDONLY); 
+    if (fd < 0) { perror("r1"); exit(1); } 
+    
+    lseek(fd, -1 * size, SEEK_END);
+    sz = read(fd, c, size); 
+    printf("called read(% d, c, 10). returned that"
+            " %d bytes were read.\n", fd, sz); 
+    c[sz] = '\0'; 
+    printf("Those bytes are as follows: % s\n", c); 
+
+    return(0);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -13,11 +40,13 @@ int main(int argc, char *argv[])
 
     printf("\n\nCopying %d MBs from %s to %s \n\n", size, sourcefilename, destinationfilename);
 
+    read_from_file_open (sourcefilename, size);
+/*
     source = fopen(sourcefilename, "r");
  
    if( source == NULL )
    {
-      printf("Press any key to exit...\n");
+      printf("can't open source file.\nPress any key to exit...\n");
       exit(EXIT_FAILURE);
    }
  
@@ -26,7 +55,7 @@ int main(int argc, char *argv[])
    if( destination == NULL )
    {
       fclose(source);
-      printf("Press any key to exit...\n");
+      printf("can't open destination file\nPress any key to exit...\n");
       exit(EXIT_FAILURE);
    }
  
@@ -47,6 +76,8 @@ int main(int argc, char *argv[])
    fclose(source);
    fclose(destination);
  
-   return 0;
+   return 0; 
+*/
 
 }
+
